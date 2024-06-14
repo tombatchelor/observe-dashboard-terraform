135c3474ed394c81da64db4bf0d67ebf75c42643
Tom Batchelor <tom@observeinc.com>
2024-06-13 17:29:41 -0700

    Clean up

--- doExport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./write_terraform.py -c ../../config.ini -e stagingdemo -w 41006584 -d 43689653 --fetch_dashboards
-./write_terraform.py -c ../../config.ini -e stagingdemo -w 41006584 -d 43689653 --fetch_datasets
-./write_terraform.py -c ../../config.ini -e stagingdemo -d 43689653 --make_locals
-./write_terraform.py -c ../../config.ini -e stagingdemo -d 43689653 --export_dashboards
